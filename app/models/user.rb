0c4e4190dd843ef9b10c10d3148f4919b95a39ab
Peter Ajayi <peterdgreat2016@gmail.com>
2024-10-22 13:21:56 +0100

    AI-10:Create models for User using devise (#1)

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :name, presence: true
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
+  validates :gender, inclusion: { in: %w[male female other] }
+  validates :fitness_level, inclusion: { in: %w[beginner intermediate advanced] }
+  validates :goals, presence: true
+end
